c8636ece2fef235cfcf133eef8ead08e7e98dba0
Luis Eduardo Romero Castro <luisinnyandell@gmail.com>
2022-10-04 14:34:16 -0500

    Update Create configs

--- src/app/pages/resultados-facturas/resultados-facturas.component.ts
+++ src/app/pages/resultados-facturas/resultados-facturas.component.ts
@@ -13,20 +13,28 @@ declare var KushkiCheckout: any;
 @Component({
   selector: 'app-resultados-facturas',
   templateUrl: './resultados-facturas.component.html',
-  styleUrls: ['./resultados-facturas.component.scss']
+  styleUrls: ['./resultados-facturas.component.scss'],
 })
 export class ResultadosFacturasComponent implements OnInit {
   private sessionId;
 
-  public clienteName: string = "";
+  public clienteName: string = '';
   public polizas: any = [];
 
-  constructor(private router: Router, private route: ActivatedRoute, config: NgbModalConfig, private modalService: NgbModal,
-    private service: ApiService, private sharing: SharingService) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    config: NgbModalConfig,
+    private modalService: NgbModal,
+    private service: ApiService,
+    private sharing: SharingService
+  ) {
     config.backdrop = 'static';
     config.keyboard = false;
     config.size = 'lg';
-    this.route.queryParams.subscribe(params => this.clienteName = params['numeroId']);
+    this.route.queryParams.subscribe(
+      (params) => (this.clienteName = params['numeroId'])
+    );
   }
 
   ngOnInit(): void {
@@ -35,10 +43,18 @@ export class ResultadosFacturasComponent implements OnInit {
   }
 
   selectall() {
-    this.polizas.map(poliza => poliza.detalle = poliza.detalle.map(data => { if (data.permite) { data.selected = !data.selected; } return data; }));
+    this.polizas.map(
+      (poliza) =>
+        (poliza.detalle = poliza.detalle.map((data) => {
+          if (data.permite) {
+            data.selected = !data.selected;
+          }
+          return data;
+        }))
+    );
   }
 
-  totalChanged() { }
+  totalChanged() {}
 
   sumarDeuda() {
     let deuda: number = 0;
@@ -70,20 +86,53 @@ export class ResultadosFacturasComponent implements OnInit {
     const reference = this.service.generateUUID();
     switch (metodo) {
       case 'wompi':
-        console.log('referencia: ' + reference, `${environment.host}/transaccion?check=wompi&rID=${reference}`);
+        console.log(
+          'referencia: ' + reference,
+          `${environment.host}/transaccion?check=wompi&rID=${reference}`
+        );
+
+        let polizasAPagar = [];
+        for (let poliza of this.polizas) {
+          for (let detalle of poliza.detalle) {
+            if (detalle.permite && detalle.selected) {
+              polizasAPagar.push(detalle);
+            }
+          }
+        }
+
         var checkout = new WidgetCheckout({
           currency: 'COP',
           amountInCents: this.sumarDeudaSeleccionada() * 100,
           reference: reference,
           publicKey: environment.wompiKey,
-          redirectUrl: `${environment.host}/transaccion?check=wompi&rID=${reference}`
+          redirectUrl: `${environment.host}/transaccion?check=wompi&rID=${reference}`,
         });
 
-        checkout.open(function (result) {
-          var transaction = result.transaction
-          console.log('Transaction ID: ', transaction.id)
-          console.log('Transaction object: ', transaction)
-        })
+        const transaccion = {
+          rID: 'a066d177-d4cd-4414-8acc-1742c7f72ce0',
+          wID: '117653-1664468208-49142',
+          jsonPolizas: JSON.stringify(polizasAPagar),
+        };
+
+        const asyncCall = new Promise((resolve, reject) =>
+          this.service.crearTransaccionWompi(transaccion, resolve, reject)
+        );
+
+        asyncCall
+          .then(() => {
+            checkout.open(function (result) {
+              var transaction = result.transaction;
+              console.log('Transaction ID: ', transaction.id);
+              console.log('Transaction object: ', transaction);
+            });
+          })
+          .catch(() =>
+            this.service.presentToast(
+              '¡Atención!',
+              'No podemos comunicarnos con la pasarela en estos momentos',
+              'warning'
+            )
+          );
 
         break;
       case 'kushki':
@@ -93,26 +142,28 @@ export class ResultadosFacturasComponent implements OnInit {
         this.modalService.open(modal);
 
         var kushki = new KushkiCheckout({
-          form: "kushki-pay-form",
-          merchant_id: "1000000530206406278515561278883",
+          form: 'kushki-pay-form',
+          merchant_id: '1000000530206406278515561278883',
           amount: {
-            "subtotalIva": 0, // Set it to 0 in case the transaction has no taxes
-            "iva": 0, // Set it to 0 in case the transaction has no taxes
-            "subtotalIva0": this.sumarDeudaSeleccionada(), // Set the total amount of the transaction here in case the it has no taxes. Otherwise, set it to 0
-            "ice": 0 // Set it to 0 in case the transaction has no ICE (Impuesto a consumos especiales)
+            subtotalIva: 0, // Set it to 0 in case the transaction has no taxes
+            iva: 0, // Set it to 0 in case the transaction has no taxes
+            subtotalIva0: this.sumarDeudaSeleccionada(), // Set the total amount of the transaction here in case the it has no taxes. Otherwise, set it to 0
+            ice: 0, // Set it to 0 in case the transaction has no ICE (Impuesto a consumos especiales)
           },
-          currency: "COP",
-          payment_methods: ["transfer"],
+          currency: 'COP',
+          payment_methods: ['transfer'],
           inTestEnvironment: true,
-          callback_url: `${environment.host}/transaccion?check=kushki&rID=${reference}`
+          callback_url: `${environment.host}/transaccion?check=kushki&rID=${reference}`,
         });
 
         break;
       case 'bancoomeva':
-        this.service.presentToast("¡Atención!", "No podemos comunicarnos con la pasarela en estos momentos", "warning");
+        this.service.presentToast(
+          '¡Atención!',
+          'No podemos comunicarnos con la pasarela en estos momentos',
+          'warning'
+        );
         break;
     }
-
   }
-
 }
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,151 +1,227 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from "rxjs/operators";
+import { map } from 'rxjs/operators';
 
 import Swal from 'sweetalert2';
 
 declare var $: any;
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-    private api = environment.api;
-    private headers = new HttpHeaders();
-    private accesstype: any = [];
-    msgSended = false;
+  private api = environment.api;
+  private headers = new HttpHeaders();
+  private accesstype: any = [];
+  msgSended = false;
 
-    constructor(private httpClient: HttpClient) {
-        this.headers.append('Access-Control-Allow-Origin', '*');
-        this.headers.append('Content-Type', 'application/json');
-        this.headers.append('No-Auth', 'True');
-    }
+  constructor(private httpClient: HttpClient) {
+    this.headers.append('Access-Control-Allow-Origin', '*');
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('No-Auth', 'True');
+  }
 
-    generateUUID() {
-        var d = new Date().getTime();
-        var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = Math.random() * 16;
-            if (d > 0) {
-                r = (d + r) % 16 | 0;
-                d = Math.floor(d / 16);
-            } else {
-                r = (d2 + r) % 16 | 0;
-                d2 = Math.floor(d2 / 16);
-            }
-            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-        })
-    }
-
-    async presentAlertConfirm(title, mensaje, additionalOpt: any = null, callback = null) {
-
-        let options = {
-            title: title,
-            text: mensaje
+  generateUUID() {
+    var d = new Date().getTime();
+    var d2 =
+      (typeof performance !== 'undefined' &&
+        performance.now &&
+        performance.now() * 1000) ||
+      0;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = Math.random() * 16;
+        if (d > 0) {
+          r = (d + r) % 16 | 0;
+          d = Math.floor(d / 16);
+        } else {
+          r = (d2 + r) % 16 | 0;
+          d2 = Math.floor(d2 / 16);
         }
+        return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
+  }
 
-        if (additionalOpt) {
-            for (let it in additionalOpt) {
-                options[it] = additionalOpt[it];
-            }
-        }
-
-        Swal.fire(options).then(callback);
-    }
-
-    async presentToast(title, mensaje, type = "success") {
-        switch (type) {
-            case "success":
-                Swal.fire({ title: title, text: mensaje, icon: "success", toast: true, showConfirmButton: false });
-                break;
-            case "danger":
-            case "error":
-                Swal.fire({ title: title, text: mensaje, icon: "error", toast: true, showConfirmButton: false });
-                break;
-            case "warning":
-                Swal.fire({ title: title, text: mensaje, icon: "warning", toast: true, showConfirmButton: false });
-                break;
-            case "info":
-                Swal.fire({ title: title, text: mensaje, icon: "info", toast: true, showConfirmButton: false });
-                break;
-            case "question":
-                Swal.fire({ title: title, text: mensaje, icon: "question", toast: true, showConfirmButton: false });
-                break;
-        }
-    }
-
-    closeDialog() {
-        Swal.close();
-    }
-
-    async doRequest(url, params, callBack, type, headers: any = {}, fallback = null) {
-
-        let load = {
-            title: 'Cargando',
-            html: 'Espere por favor...',
-            allowOutsideClick: false,
-            allowEnterKey: false,
-            allowEscapeKey: false,
-            showConfirmButton: false,
-            willOpen: () => {
-                Swal.showLoading();
-            }
-        }
-
-        if (!params.hasOwnProperty("DisableLoad")) Swal.fire(load);
-        if (typeof params == "string") if (!params.includes("DisableLoad")) Swal.fire(load);
-
-        const errorHandler = (er) => {
-            let disableEr = false;
-            if (params.hasOwnProperty("DisableEr")) disableEr = true;
-            if (typeof params == "string") if (params.includes("DisableEr")) disableEr = true;
-            try {
-                er = JSON.stringify(er);
-                if (!disableEr) this.presentToast("Error: ", er, "error");
-            } catch (e) {
-                if (!disableEr) this.presentToast("Error: ", er, "error");
-            }
-            fallback && fallback(er);
-        }
-
-        switch (type.toUpperCase()) {
-
-            case 'GET':
-                this.httpClient.get(url, headers)
-                    .pipe(map(resp => resp))
-                    .subscribe(callBack, errorHandler);
-                break;
-
-            case 'POST':
-                this.httpClient.post(url, params, headers)
-                    .pipe(map(resp => resp))
-                    .subscribe(callBack, errorHandler);
-                break;
-        }
-
+  async presentAlertConfirm(
+    title,
+    mensaje,
+    additionalOpt: any = null,
+    callback = null
+  ) {
+    let options = {
+      title: title,
+      text: mensaje,
     };
 
-    getUserClaims() {
-        let parsedSession = "PUBLIC";
-        return new HttpHeaders({
-            'Authorization': 'Bearer ' + parsedSession,
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest'
+    if (additionalOpt) {
+      for (let it in additionalOpt) {
+        options[it] = additionalOpt[it];
+      }
+    }
+
+    Swal.fire(options).then(callback);
+  }
+
+  async presentToast(title, mensaje, type = 'success') {
+    switch (type) {
+      case 'success':
+        Swal.fire({
+          title: title,
+          text: mensaje,
+          icon: 'success',
+          toast: true,
+          showConfirmButton: false,
         });
-
+        break;
+      case 'danger':
+      case 'error':
+        Swal.fire({
+          title: title,
+          text: mensaje,
+          icon: 'error',
+          toast: true,
+          showConfirmButton: false,
+        });
+        break;
+      case 'warning':
+        Swal.fire({
+          title: title,
+          text: mensaje,
+          icon: 'warning',
+          toast: true,
+          showConfirmButton: false,
+        });
+        break;
+      case 'info':
+        Swal.fire({
+          title: title,
+          text: mensaje,
+          icon: 'info',
+          toast: true,
+          showConfirmButton: false,
+        });
+        break;
+      case 'question':
+        Swal.fire({
+          title: title,
+          text: mensaje,
+          icon: 'question',
+          toast: true,
+          showConfirmButton: false,
+        });
+        break;
     }
+  }
 
-    getFacturas(userData: string, callBack) {
-        this.doRequest(`${this.api}/consultas/consultarPoliza.php?numeroId=${userData}`, { DisableLoad: true }, callBack, 'get', { headers: this.getUserClaims() });
+  closeDialog() {
+    Swal.close();
+  }
+
+  async doRequest(
+    url,
+    params,
+    callBack,
+    type,
+    headers: any = {},
+    fallback = null
+  ) {
+    let load = {
+      title: 'Cargando',
+      html: 'Espere por favor...',
+      allowOutsideClick: false,
+      allowEnterKey: false,
+      allowEscapeKey: false,
+      showConfirmButton: false,
+      willOpen: () => {
+        Swal.showLoading();
+      },
+    };
+
+    if (!params.hasOwnProperty('DisableLoad')) Swal.fire(load);
+    if (typeof params == 'string')
+      if (!params.includes('DisableLoad')) Swal.fire(load);
+
+    const errorHandler = (er) => {
+      let disableEr = false;
+      if (params.hasOwnProperty('DisableEr')) disableEr = true;
+      if (typeof params == 'string')
+        if (params.includes('DisableEr')) disableEr = true;
+      try {
+        er = JSON.stringify(er);
+        if (!disableEr) this.presentToast('Error: ', er, 'error');
+      } catch (e) {
+        if (!disableEr) this.presentToast('Error: ', er, 'error');
+      }
+      fallback && fallback(er);
+    };
+
+    switch (type.toUpperCase()) {
+      case 'GET':
+        this.httpClient
+          .get(url, headers)
+          .pipe(map((resp) => resp))
+          .subscribe(callBack, errorHandler);
+        break;
+
+      case 'POST':
+        this.httpClient
+          .post(url, params, headers)
+          .pipe(map((resp) => resp))
+          .subscribe(callBack, errorHandler);
+        break;
     }
+  }
 
-    getEstadoTransaccionWompi(transaction_id: string, callBack, fallback) {
-        this.doRequest(`${environment.wompiServer}/transactions/${transaction_id}`, { DisableLoad: true, DisableEr: true }, callBack, 'get', {}, fallback);
-    }
+  getUserClaims() {
+    let parsedSession = 'PUBLIC';
+    return new HttpHeaders({
+      Authorization: 'Bearer ' + parsedSession,
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+    });
+  }
 
-    notifyWompiBack(transactionData: any) {
-        this.doRequest(`${environment.api}/aplicarRecaudo/aplicarWompi`, { DisableLoad: true, DisableEr: true, ...transactionData }, () => { }, 'post');
-    }
+  getFacturas(userData: string, callBack) {
+    this.doRequest(
+      `${this.api}/consultas/consultarPoliza?numeroId=${userData}`,
+      { DisableLoad: true },
+      callBack,
+      'get',
+      { headers: this.getUserClaims() }
+    );
+  }
 
+  getEstadoTransaccionWompi(transaction_id: string, callBack, fallback) {
+    this.doRequest(
+      `${environment.wompiServer}/transactions/${transaction_id}`,
+      { DisableLoad: true, DisableEr: true },
+      callBack,
+      'get',
+      {},
+      fallback
+    );
+  }
+
+  notifyWompiBack(transactionData: any) {
+    this.doRequest(
+      `${environment.api}/aplicarRecaudo/aplicarWompi`,
+      { DisableLoad: true, DisableEr: true, ...transactionData },
+      () => {},
+      'post'
+    );
+  }
+
+  crearTransaccionWompi(transactionData: any, callback, fallback) {
+    this.doRequest(
+      `${environment.api}/aplicarRecaudo/crearTransaccion`,
+      { DisableLoad: true, DisableEr: true, ...transactionData },
+      callback,
+      'post',
+      {},
+      fallback
+    );
+  }
 }
