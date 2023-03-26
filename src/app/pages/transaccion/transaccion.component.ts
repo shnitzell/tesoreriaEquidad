3c6dc557b0fbe87ab7ad836831a877dba3cd2656
Luis Eduardo Romero Castro <luisinnyandell@gmail.com>
2023-03-25 19:54:41 -0500

    Added extra space

--- src/app/pages/transaccion/transaccion.component.ts
+++ src/app/pages/transaccion/transaccion.component.ts
@@ -8,88 +8,95 @@ import { ApiService } from '../../../services/api.service';
 @Component({
   selector: 'app-transaccion',
   templateUrl: './transaccion.component.html',
-  styleUrls: ['./transaccion.component.scss']
+  styleUrls: ['./transaccion.component.scss'],
 })
 export class TransaccionComponent implements OnInit {
-  public checkoutMethod: string = "";
-  public resultID: string = "";
-  public transactionID: string = "";
+  public checkoutMethod: string = '';
+  public resultID: string = '';
+  public transactionID: string = '';
 
-  public estadoTransaccion: string = "";
+  public estadoTransaccion: string = '';
   public valorPago: number = 0;
-  public fechaPago = "";
-  public desdePago = "";
-  public productoPago = "";
+  public fechaPago = '';
+  public desdePago = '';
+  public productoPago = '';
 
-  public state: string = "p";
+  public state: string = 'p';
 
   public genPDF: boolean = false;
 
-  constructor(private router: Router, private route: ActivatedRoute, private service: ApiService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private service: ApiService
+  ) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.checkoutMethod = params["check"];
-      this.resultID = params["rID"];
-      this.transactionID = params["id"];
+    this.route.queryParams.subscribe((params) => {
+      this.checkoutMethod = params['check'];
+      this.resultID = params['rID'];
+      this.transactionID = params['id'];
 
-      this.service.getEstadoTransaccionWompi(this.transactionID, (wompiResponse) => {
+      this.service.getEstadoTransaccionWompi(
+        this.transactionID,
+        (wompiResponse) => {
+          this.service.notifyWompiBack({
+            check: this.checkoutMethod,
+            rID: this.resultID,
+            id: this.transactionID,
+          });
 
-        this.service.notifyWompiBack({
-          check: this.checkoutMethod,
-          rID: this.resultID,
-          id: this.transactionID
-        });
+          this.valorPago = wompiResponse.data.amount_in_cents / 100;
+          this.fechaPago = wompiResponse.data.created_at;
+          this.desdePago = wompiResponse.data.payment_method_type;
+          this.productoPago =
+            wompiResponse.data.payment_method.payment_description;
 
-        this.valorPago = wompiResponse.data.amount_in_cents;
-        this.fechaPago = wompiResponse.data.created_at;
-        this.desdePago = wompiResponse.data.payment_method_type;
-        this.productoPago = wompiResponse.data.payment_method.payment_description;
-
-        switch (wompiResponse.data.status) {
-          case "PENDING":
-            this.state = 'p'
-            setTimeout(() => this.ngOnInit(), 5000);
-            break;
-          case "APPROVED":
-            this.state = 'e'
-            break;
-          case "DECLINED":
-            this.state = 'd'
-            break;
-          case "VOIDED":
-            this.state = 'v'
-            break;
-          default:
-            this.state = 'f'
-            break;
+          switch (wompiResponse.data.status) {
+            case 'PENDING':
+              this.state = 'p';
+              setTimeout(() => this.ngOnInit(), 5000);
+              break;
+            case 'APPROVED':
+              this.state = 'e';
+              break;
+            case 'DECLINED':
+              this.state = 'd';
+              break;
+            case 'VOIDED':
+              this.state = 'v';
+              break;
+            default:
+              this.state = 'f';
+              break;
+          }
+        },
+        () => {
+          this.state = 'f';
+          this.fechaPago = new Date().toISOString();
+          this.desdePago = 'Ningún banco';
+          this.productoPago = 'No existe transacción';
         }
-
-      }, () => {
-        this.state = 'f';
-        this.fechaPago = new Date().toISOString();
-        this.desdePago = "Ningún banco";
-        this.productoPago = "No existe transacción"
-      });
+      );
     });
   }
 
   getEstatus() {
     switch (this.state) {
       case 'e':
-        this.estadoTransaccion = 'fue exitoso'
+        this.estadoTransaccion = 'fue exitoso';
         break;
       case 'p':
-        this.estadoTransaccion = 'está pendiente'
+        this.estadoTransaccion = 'está pendiente';
         break;
       case 'f':
-        this.estadoTransaccion = 'ha fallado'
+        this.estadoTransaccion = 'ha fallado';
         break;
       case 'd':
-        this.estadoTransaccion = 'fue declinado'
+        this.estadoTransaccion = 'fue declinado';
         break;
       case 'v':
-        this.estadoTransaccion = 'fue anulado'
+        this.estadoTransaccion = 'fue anulado';
         break;
     }
 
@@ -102,22 +109,35 @@ export class TransaccionComponent implements OnInit {
     const doc = new jsPDF('p', 'pt', 'a4');
     const options = {
       background: 'white',
-      scale: 3
+      scale: 3,
     };
 
-    html2canvas(DATA, options).then((canvas) => {
-      const img = canvas.toDataURL('image/PNG');
+    html2canvas(DATA, options)
+      .then((canvas) => {
+        const img = canvas.toDataURL('image/PNG');
 
-      const bufferX = 15;
-      const bufferY = 15;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      return doc;
-    }).then((docResult) => {
-      this.genPDF = false;
-      docResult.save(`transaction_${this.transactionID}_${new Date().toISOString()}.pdf`);
-    });
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(
+          img,
+          'PNG',
+          bufferX,
+          bufferY,
+          pdfWidth,
+          pdfHeight,
+          undefined,
+          'FAST'
+        );
+        return doc;
+      })
+      .then((docResult) => {
+        this.genPDF = false;
+        docResult.save(
+          `transaction_${this.transactionID}_${new Date().toISOString()}.pdf`
+        );
+      });
   }
 }
