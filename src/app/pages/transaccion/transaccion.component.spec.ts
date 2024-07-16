ffcc031ea87e0b65737668c004312ea58d5a7cd6
Luis Eduardo Romero Castro <luisinnyandell@gmail.com>
2024-07-16 16:51:55 -0500

    Update to Angular 11

--- src/app/pages/transaccion/transaccion.component.spec.ts
+++ src/app/pages/transaccion/transaccion.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TransaccionComponent } from './transaccion.component';
 
@@ -6,7 +6,7 @@ describe('TransaccionComponent', () => {
   let component: TransaccionComponent;
   let fixture: ComponentFixture<TransaccionComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TransaccionComponent ]
     })
